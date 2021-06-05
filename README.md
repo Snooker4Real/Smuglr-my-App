665070d8ff611f9a857ea08d541639e163121ea7
Snooker4Real <cindanojonathan@icloud.com>
2021-06-05 18:32:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Smuglr-my-App
---
+++ Smuglr/TpisView.swift
@@ -0,0 +1,20 @@
+//
+//  TpisView.swift
+//  Smuglr
+//
+//  Created by Jonathan Cindano Mwamba on 05/06/2021.
+//
+
+import SwiftUI
+
+struct TpisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TpisView_Previews: PreviewProvider {
+    static var previews: some View {
+        TpisView()
+    }
+}
