f81bac533458cb9b8f141072a5e4500f06086f95
Snooker4Real <cindanojonathan@icloud.com>
2021-06-05 12:43:45 +0200

    Initial Commit

---
+++ Smuglr/SmuglrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmuglrApp.swift
+//  Smuglr
+//
+//  Created by Jonathan Cindano Mwamba on 05/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SmuglrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
